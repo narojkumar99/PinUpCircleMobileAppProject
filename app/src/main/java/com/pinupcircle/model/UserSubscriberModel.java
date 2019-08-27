3f0a5af800302da062042bd6b0dedbe3196a3a62
SutirthaSaha <sutirthasaha12@gmail.com>
2019-08-27 16:24:52 +0530

    Subscriber Model Added

--- app/src/main/java/com/pinupcircle/model/UserSubscriberModel.java
+++ app/src/main/java/com/pinupcircle/model/UserSubscriberModel.java
@@ -1,5 +1,220 @@
 package com.pinupcircle.model;
 
-public class UserSubscriberModel {
+import java.util.ArrayList;
+import java.util.List;
 
+class UserPin{
+    private String userPin;
+
+    public UserPin(String userPin) {
+        this.userPin = userPin;
+    }
+
+    public String getUserPin() {
+        return userPin;
+    }
+
+    public void setUserPin(String userPin) {
+        this.userPin = userPin;
+    }
+}
+class UserNewsPreference{
+    private String userNewsPreference;
+
+    public UserNewsPreference(String userNewsPreference) {
+        this.userNewsPreference = userNewsPreference;
+    }
+
+    public String getUserNewsPreference() {
+        return userNewsPreference;
+    }
+
+    public void setUserNewsPreference(String userNewsPreference) {
+        this.userNewsPreference = userNewsPreference;
+    }
+}
+class UserOTP{
+    private String userOTP;
+
+    public UserOTP(String userOTP) {
+        this.userOTP = userOTP;
+    }
+
+    public String getUserOTP() {
+        return userOTP;
+    }
+
+    public void setUserOTP(String userOTP) {
+        this.userOTP = userOTP;
+    }
+}
+class UserSocialInterest{
+    private String userSocialInterest;
+
+    public UserSocialInterest(String userSocialInterest) {
+        this.userSocialInterest = userSocialInterest;
+    }
+
+    public String getUserSocialInterest() {
+        return userSocialInterest;
+    }
+
+    public void setUserSocialInterest(String userSocialInterest) {
+        this.userSocialInterest = userSocialInterest;
+    }
+}
+class UserAddress{
+    private String streetAddress,city,state,zip;
+
+    public UserAddress(String streetAddress, String city, String state, String zip) {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+}
+public class UserSubscriberModel {
+    private String userName,userPhone,userAge,userEmail,userPickRef,userCountryCode,bonusPoints;
+    private List<UserPin> userPins;
+    private List<UserNewsPreference> userNewsPreferences;
+    private UserOTP otpEntity;
+    private List<UserAddress> userAddresses;
+    private List<UserSocialInterest> userSocialInterests;
+
+    public UserSubscriberModel() {
+        userPins=new ArrayList<>();
+    }
+
+    public List<UserPin> getUserPins() {
+        return userPins;
+    }
+
+    public void addUserPins(String userPin) {
+        UserPin userPinObj=new UserPin(userPin);
+        this.userPins.add(userPinObj);
+    }
+
+    public List<UserNewsPreference> getUserNewsPreferences() {
+        return userNewsPreferences;
+    }
+
+    public void addUserNewsPreferences(String newsPreference) {
+        UserNewsPreference userNewsPreference=new UserNewsPreference(newsPreference);
+        userNewsPreferences.add(userNewsPreference);
+    }
+
+    public UserOTP getOtpEntity() {
+        return otpEntity;
+    }
+
+    public void setOtpEntity(String otp) {
+        UserOTP userOTP=new UserOTP(otp);
+        this.otpEntity=userOTP;
+    }
+
+    public List<UserAddress> getUserAddresses() {
+        return userAddresses;
+    }
+
+    public void addUserAddresses(String streetAddress,String city,String state,String zip) {
+        UserAddress address=new UserAddress(streetAddress,city,state,zip);
+        userAddresses.add(address);
+    }
+
+    public List<UserSocialInterest> getUserSocialInterests() {
+        return userSocialInterests;
+    }
+
+    public void addUserSocialInterests(String userSocialInterest) {
+        UserSocialInterest socialInterest=new UserSocialInterest(userSocialInterest);
+        userSocialInterests.add(socialInterest);
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(String userAge) {
+        this.userAge = userAge;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPickRef() {
+        return userPickRef;
+    }
+
+    public void setUserPickRef(String userPickRef) {
+        this.userPickRef = userPickRef;
+    }
+
+    public String getUserCountryCode() {
+        return userCountryCode;
+    }
+
+    public void setUserCountryCode(String userCountryCode) {
+        this.userCountryCode = userCountryCode;
+    }
+
+    public String getBonusPoints() {
+        return bonusPoints;
+    }
+
+    public void setBonusPoints(String bonusPoints) {
+        this.bonusPoints = bonusPoints;
+    }
 }
--- app/src/main/java/com/pinupcircle/ui/subscriber/Subscriber.java
+++ app/src/main/java/com/pinupcircle/ui/subscriber/Subscriber.java
@@ -5,13 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
 import com.pinupcircle.R;
+import com.pinupcircle.model.UserSubscriberModel;
 import com.pinupcircle.ui.otpVerify.OTPAuthenticaton;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Subscriber extends AppCompatActivity {
+
+    final String url="http://13.59.60.142:8080/users/registerUser?userdef=abc";
+    UserSubscriberModel subscriberModel;
     ImageView imgNext;
+    EditText editTextSubscriberName,editTextSubscriberAddress,editTextMobileNumber,editTextLocality,editTextPinCode,editTextSubsriberEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +40,45 @@ public class Subscriber extends AppCompatActivity {
         });
     }
     private void initFields() {
+        subscriberModel=new UserSubscriberModel();
         imgNext = (ImageView) findViewById(R.id.imgNext);
+        editTextSubscriberName=findViewById(R.id.editTextSubscriberName);
+        editTextLocality=findViewById(R.id.editTextLocality);
+        editTextMobileNumber=findViewById(R.id.editTextMobileNumber);
+        editTextPinCode=findViewById(R.id.editTextPinCode);
+        editTextSubscriberAddress=findViewById(R.id.editTextSubscriberAddress);
+        editTextSubsriberEmail=findViewById(R.id.editTextSubscriberEmail);
+    }
+
+    public void onLoginClick(View view) {
+        subscriberModel.setUserName(editTextSubscriberName.getText().toString().trim());
+        subscriberModel.setUserPhone(editTextMobileNumber.getText().toString().trim());
+        subscriberModel.setUserAge("34");
+        subscriberModel.setUserEmail(editTextSubsriberEmail.getText().toString().trim());
+        subscriberModel.setUserPickRef("c://image");
+        subscriberModel.setUserCountryCode("IN");
+        subscriberModel.setBonusPoints("100");
+        subscriberModel.addUserPins(editTextPinCode.getText().toString());
+        subscriberModel.setOtpEntity("123456789");
+        subscriberModel.addUserAddresses(editTextSubscriberAddress.toString().trim(),"Kokata","Salt Lake","700108");
+        subscriberModel.addUserSocialInterests("None");
+
+        Gson gson=new Gson();
+        try {
+            JSONObject requestObject=new JSONObject(gson.toJson(subscriberModel));
+            JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.POST, url,requestObject,new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Toast.makeText(Subscriber.this, response.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
